b2da78a98f4da44ae7da22c2b780244a92df31e7
Kevin Giordani <giordani.kevin.02@gmail.com>
2021-04-28 16:19:20 +0200

    v4

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  const commitsToCheck = github.context.payload.commits.map(
-    (commit) => commit.id
-  );
-  console.log("Commits to check", commitsToCheck);
-
-  //  --quiet: exits with 1 if there were differences (https://git-scm.com/docs/git-diff)
-  for await (const commitId of commitsToCheck) {
-    const exitCode = await exec.exec(
-      "git",
-      ["diff", "--quiet", commitId, "--", "a/"],
-      {
-        ignoreReturnCode: true,
-        silent: false,
-        cwd: getCWD(),
-      }
+const main = async () => {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput("who-to-greet");
+    console.log(`Hello ${nameToGreet}!`);
+    const time = new Date().toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    const commitsToCheck = github.context.payload.commits.map(
+      (commit) => commit.id
     );
-    console.log("Commit", commitId, "ended diff in", exitCode);
-  }
+    console.log("Commits to check", commitsToCheck);
 
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+    //  --quiet: exits with 1 if there were differences (https://git-scm.com/docs/git-diff)
+    for await (const commitId of commitsToCheck) {
+      const exitCode = await exec.exec(
+        "git",
+        ["diff", "--quiet", commitId, "--", "a/"],
+        {
+          ignoreReturnCode: true,
+          silent: false,
+          cwd: getCWD(),
+        }
+      );
+      console.log("Commit", commitId, "ended diff in", exitCode);
+    }
+
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+main();
